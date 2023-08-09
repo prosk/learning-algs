62067c55b43525ab62e4a9223f7f5add81e07d45
prosk <alexey.proskurnin@gmail.com>
2023-08-09 14:16:01 +0500

    Yandex training 1 lesson 2 - NearestNumber

---
+++ src/com/mycompany/yandex/training10/lesson2/NearestNumber.java
@@ -0,0 +1,78 @@
+package com.mycompany.yandex.training10.lesson2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.Locale;
+import java.util.StringTokenizer;
+
+public class NearestNumber {
+    final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    final PrintWriter out = new PrintWriter(System.out);
+    StringTokenizer tok = new StringTokenizer("");
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        new NearestNumber().run();
+    }
+
+    private void run() {
+        try {
+            long timeStart = System.currentTimeMillis();
+            solve();
+            out.close();
+            long timeEnd = System.currentTimeMillis();
+            System.err.println("Time(ms) = " + (timeEnd - timeStart));
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void solve() {
+        int n = readInt();
+        int[] arr = readIntArray(n);
+        int target = readInt();
+
+        int ans = arr[0];
+        int currDiff = Math.abs(target - arr[0]);
+        int newDiff;
+        for(int i = 1; i < arr.length; i++) {
+            newDiff = Math.abs(target - arr[i]);
+            if (newDiff < currDiff) {
+                currDiff = newDiff;
+                ans = arr[i];
+            }
+        }
+        out.println(ans);
+    }
+
+    private int[] readIntArray(int n) {
+        int[] a = new int[n];
+        for (int i = 0; i < n; ++i) a[i] = readInt();
+        return a;
+    }
+
+    private int readInt() {
+        return Integer.parseInt(readString());
+    }
+
+    private String readString() {
+        while (!tok.hasMoreTokens()) {
+            String nextLine = readLine();
+            if (null == nextLine) return null;
+            tok = new StringTokenizer(nextLine);
+        }
+
+        return tok.nextToken();
+    }
+
+    private String readLine() {
+        try {
+            return br.readLine();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
