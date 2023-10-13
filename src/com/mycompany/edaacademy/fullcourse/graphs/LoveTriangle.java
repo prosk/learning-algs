765862b0f6dec78c55bc207c627669b934fb39e4
prosk <alexey.proskurnin@gmail.com>
2023-10-13 18:07:32 +0300

    eda graphs

---
+++ src/com/mycompany/edaacademy/fullcourse/graphs/LoveTriangle.java
@@ -0,0 +1,74 @@
+package com.mycompany.edaacademy.fullcourse.graphs;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.Locale;
+import java.util.StringTokenizer;
+
+public class LoveTriangle {
+    final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    final PrintWriter out = new PrintWriter(System.out);
+    StringTokenizer tok = new StringTokenizer("");
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        new LoveTriangle().run();
+    }
+
+    private void run() {
+        try {
+            long timeStart = System.currentTimeMillis();
+            solve();
+            out.close();
+            long timeEnd = System.currentTimeMillis();
+            System.err.println("Time(ms) = " + (timeEnd - timeStart));
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void solve() {
+        int n = readInt();
+        int[] inp = new int[n+1];
+        for(int i = 1; i <= n; i++) {
+            inp[i] = readInt();
+        }
+
+        int a, b, c;
+        for(int i = 1; i <= n; i++) {
+            a = i;
+            b = inp[a];
+            c = inp[b];
+            if (inp[c] == a) {
+                out.println("YES");
+                return;
+            }
+        }
+        out.println("NO");
+    }
+
+    private int readInt() {
+        return Integer.parseInt(readString());
+    }
+
+    private String readString() {
+        while (!tok.hasMoreTokens()) {
+            String nextLine = readLine();
+            if (null == nextLine) return null;
+            tok = new StringTokenizer(nextLine);
+        }
+
+        return tok.nextToken();
+    }
+
+    private String readLine() {
+        try {
+            return br.readLine();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
