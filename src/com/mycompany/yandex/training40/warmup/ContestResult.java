5c079cf5eff5a692067e857308fe363b73f16a60
prosk <alexey.proskurnin@gmail.com>
2023-11-02 09:28:19 +0300

    yandex 4 - ContestResult

---
+++ src/com/mycompany/yandex/training40/warmup/ContestResult.java
@@ -0,0 +1,67 @@
+package com.mycompany.yandex.training40.warmup;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.Locale;
+import java.util.StringTokenizer;
+
+public class ContestResult {
+    final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    final PrintWriter out = new PrintWriter(System.out);
+    StringTokenizer tok = new StringTokenizer("");
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        new ContestResult().run();
+    }
+
+    private void run() {
+        try {
+            long timeStart = System.currentTimeMillis();
+            solve();
+            out.close();
+            long timeEnd = System.currentTimeMillis();
+            System.err.println("Time(ms) = " + (timeEnd - timeStart));
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void solve() {
+        int a = readInt();
+        int b = readInt();
+        int n = readInt();
+
+        int maxA = a;
+        int qB = b / n, rB = b % n;
+        int minB = (rB == 0) ? qB : qB+1;
+        String ans = (maxA > minB) ? "Yes" : "No";
+
+        out.println(ans);
+    }
+
+    private int readInt() {
+        return Integer.parseInt(readString());
+    }
+
+    private String readString() {
+        while (!tok.hasMoreTokens()) {
+            String nextLine = readLine();
+            if (null == nextLine) return null;
+            tok = new StringTokenizer(nextLine);
+        }
+
+        return tok.nextToken();
+    }
+
+    private String readLine() {
+        try {
+            return br.readLine();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
