fff321ede82174967f4f306edac68c3acaed399b
alex.proskurnin <alexey.proskurnin@gmail.com>
2024-01-01 17:08:53 +0300

    short solution template with IO

---
+++ src/com/mycompany/templates/Solution.java
@@ -0,0 +1,47 @@
+package com.mycompany.templates;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class Solution {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static PrintWriter out = new PrintWriter(System.out);
+    static StringTokenizer tok = new StringTokenizer("");
+
+    public static void main(String[] args) {
+        new Solution().run();
+        out.close();
+    }
+
+    void run() {
+        int a = readInt();
+        int b = readInt();
+        int ans = a + b;
+        out.println(ans);
+    }
+
+    int readInt() {
+        return Integer.parseInt(readString());
+    }
+
+    String readString() {
+        while (!tok.hasMoreTokens()) {
+            String nextLine = readLine();
+            if (null == nextLine) return null;
+            tok = new StringTokenizer(nextLine);
+        }
+
+        return tok.nextToken();
+    }
+
+    String readLine() {
+        try {
+            return br.readLine();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
