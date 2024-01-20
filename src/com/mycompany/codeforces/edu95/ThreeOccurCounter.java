70032c208fa304929e388d7451a0c08ef0d61dc5
prosk <alexey.proskurnin@gmail.com>
2024-01-20 14:32:06 +0300

    codeforces edu round 95 ThreeOccurCounter template

---
+++ src/com/mycompany/codeforces/edu95/ThreeOccurCounter.java
@@ -0,0 +1,57 @@
+package com.mycompany.codeforces.edu95;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.Locale;
+import java.util.StringTokenizer;
+
+public class ThreeOccurCounter {
+    final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    final PrintWriter out = new PrintWriter(System.out);
+    StringTokenizer tok = new StringTokenizer("");
+
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        new ThreeOccurCounter().run();
+    }
+
+    private void run() {
+        solve();
+        out.close();
+    }
+
+    private void solve() {
+        int n = readInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++) {
+            arr[i] = readInt();
+        }
+
+
+    }
+
+    private int readInt() {
+        return Integer.parseInt(readString());
+    }
+
+    private String readString() {
+        while (!tok.hasMoreTokens()) {
+            String nextLine = readLine();
+            if (null == nextLine) return null;
+            tok = new StringTokenizer(nextLine);
+        }
+
+        return tok.nextToken();
+    }
+
+    private String readLine() {
+        try {
+            return br.readLine();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
