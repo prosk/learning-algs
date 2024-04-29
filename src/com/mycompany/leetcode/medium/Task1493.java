a3a3f44b90ce93a453962a014fd595a49e413f7e
prosk <alexey.proskurnin@gmail.com>
2024-04-29 18:45:31 +0300

    leetcode - medium Task1493 - solution with sliding window (two pointers)

--- src/com/mycompany/leetcode/medium/Task1493.java
+++ src/com/mycompany/leetcode/medium/Task1493.java
@@ -7,9 +7,9 @@ public class Task1493 {
         int[] test1 = {1,1,0,1}, test2 = {0,1,1,1,0,1,1,0,1}, test3 = {1,1,1};
 
 
-        int ans1 = task.longestSubarray(test1);
-        int ans2 = task.longestSubarray(test2);
-        int ans3 = task.longestSubarray(test3);
+        int ans1 = task.longestSubarray2(test1);
+        int ans2 = task.longestSubarray2(test2);
+        int ans3 = task.longestSubarray2(test3);
 
         System.out.println(ans1 + " " + ans2 + " " + ans3);
     }
@@ -35,6 +35,28 @@ public class Task1493 {
                 }
             }
         }
-        return (maxLen == nums.length) ? maxLen - 1 : maxLen ;
+        return (maxLen == nums.length) ? maxLen - 1 : maxLen;
+    }
+
+    // int[] test1 = {1,1,0,1}, test2 = {0,1,1,1,0,1,1,0,1}, test3 = {1,1,1};
+    public int longestSubarray2(int[] nums) {
+        // Maintain a sliding window where there is at most one zero on it.
+        int left = 0, zeroCnt = 0;
+        int currLen = 0, maxLen = 0, lastZeroIndex = -1;
+        for(int right = 0; right < nums.length; right++) {
+            zeroCnt += (1 - nums[right]);
+
+            if (zeroCnt <= 1) {
+                currLen = right - left + 1 - zeroCnt;
+                maxLen = Math.max(maxLen, currLen);
+            } else {
+                left = lastZeroIndex + 1;
+                zeroCnt--;
+            }
+
+            if (nums[right] == 0)
+                lastZeroIndex = right;
+        }
+        return (maxLen == nums.length) ? maxLen - 1 : maxLen;
     }
 }
