a90c4f332ae8e7e09236db768307550da58cf6a5
prosk <alexey.proskurnin@gmail.com>
2024-09-24 12:08:30 +0300

    cses problem set - Bit Strings

---
+++ src/com/mycompany/cses/problemset/BitStrings.java
@@ -0,0 +1,50 @@
+// package com.mycompany.cses.problemset;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class BitStrings {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static PrintWriter out = new PrintWriter(System.out);
+    static StringTokenizer tok = new StringTokenizer("");
+
+    public static void main(String[] args) {
+        new BitStrings().run();
+        out.close();
+    }
+
+    void run() {
+        int n = readInt();
+        int modulo = 1_000_000_000 + 7;
+        int ans = 1;
+        for(int i = 0; i < n; i++) {
+            ans = (ans * 2) % modulo;
+        }
+        out.println(ans);
+    }
+
+    int readInt() {
+        return Integer.parseInt(readString());
+    }
+
+    String readString() {
+        while (!tok.hasMoreTokens()) {
+            String nextLine = readLine();
+            if (null == nextLine) return null;
+            tok = new StringTokenizer(nextLine);
+        }
+
+        return tok.nextToken();
+    }
+
+    String readLine() {
+        try {
+            return br.readLine();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
