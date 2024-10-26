3fd7dcc418a2a82bd186eeda82eac9b02f8084d3
prosk <alexey.proskurnin@gmail.com>
2024-10-26 17:03:55 +0300

    yandex training 6

--- src/com/mycompany/yandex/training60/testing/LettersOnTheScreen.java
+++ src/com/mycompany/yandex/training60/testing/LettersOnTheScreen.java
@@ -1,4 +1,4 @@
-package com.mycompany.yandex.training60.testing;
+// package com.mycompany.yandex.training60.testing;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -36,6 +36,8 @@ public class LettersOnTheScreen {
         boolean isO = isLetterO(screen, n);
         boolean isC = isLetterC(screen, n);
         boolean isL = isLetterL(screen, n);
+        boolean isH = isLetterH(screen, n);
+        boolean isP = isLetterP(screen, n);
         if (isI) {
             out.println("I");
         } else if (isO) {
@@ -44,9 +46,127 @@ public class LettersOnTheScreen {
             out.println("C");
         } else if(isL) {
             out.println("L");
+        } else if(isH) {
+            out.println("H");
+        } else if (isP) {
+            out.println("P");
         } else {
             out.println("X");
         }
+    }
+
+    boolean isLetterP(int[][] screen, int n) {
+        for(int fW = 1; fW <= n; fW++) {
+            for(int fH = 1; fH <= n; fH++) {
+                for(int fStartRow = 1; fStartRow <= n - fH + 1; fStartRow++) {
+                    for(int fStartCol = 1; fStartCol <= n - fW + 1; fStartCol++) {
+                        boolean res = isFilledByVal(screen, n, fStartRow, fStartCol, fW, fH, 0);
+                        if (res && screen[fStartRow][fStartCol-1] == 1 &&
+                            screen[fStartRow][fStartCol+fW] == 1 && screen[fStartRow+fH][fStartCol] == 1) {
+                            // first is found, trying to find second
+                            int sStartCol = fStartCol;
+                            for(int sW = fW+1; sW <= n; sW++) {
+                                for (int sH = 1; sH <= n; sH++) {
+                                    for (int sStartRow = fStartRow + fH + 1; sStartRow <= n - sH + 1; sStartRow++) {
+                                        res = isFilledByVal(screen, n, sStartRow, sStartCol, sW, sH, 0);
+                                        if (res && screen[sStartRow][sStartCol - 1] == 1) {
+                                            int copyOfScreen[][] = getScreenCopy(screen, n);
+                                            fillByVal(copyOfScreen, n, fStartRow, fStartCol, fW, fH, 1);
+                                            fillByVal(copyOfScreen, n, sStartRow, sStartCol, sW, sH, 1);
+                                            boolean pIsFound = isFilledAndCoverLikeP(copyOfScreen, n,
+                                                fStartRow, fStartCol, fW, fH,
+                                                sStartRow, sStartCol, sW, sH);
+                                            if (pIsFound) return true;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    boolean isFilledAndCoverLikeP(int[][] screen, int n,
+                                  int inFirstStartRow, int inFirstStartCol, int inFirstW, int inFirstH,
+                                  int inSecondStartRow, int inSecondStartCol, int inSecondW, int inSecondH) {
+        int sum = getSumOfScreen(screen, n);
+        for(int w = 1; w <= n; w++) {
+            for(int h = 1; h <= n; h++) {
+                for(int startRow = 1; startRow <= n - h + 1; startRow++) {
+                    for(int startCol = 1; startCol <= n - w + 1; startCol++) {
+                        boolean res = isFilledByValAndSum(screen, n, startRow, startCol, w, h, 1, sum);
+                        if (res) {
+                            int endRow = startRow + h - 1;
+                            int endCol = startCol + w - 1;
+                            int inEndRow = inSecondStartRow + inSecondH - 1;
+                            int inEndCol = inSecondStartCol + inSecondW - 1;
+                            return (startRow < inFirstStartRow && startCol < inFirstStartCol
+                                && endRow == inEndRow && endCol == inEndCol);
+                        }
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    boolean isLetterH(int[][] screen, int n) {
+        for(int fW = 1; fW <= n; fW++) {
+            for(int fH = 1; fH <= n; fH++) {
+                for(int fStartRow = 1; fStartRow <= n - fH + 1; fStartRow++) {
+                    for(int fStartCol = 1; fStartCol <= n - fW + 1; fStartCol++) {
+                        boolean res = isFilledByVal(screen, n, fStartRow, fStartCol, fW, fH, 0);
+                        if (res && screen[fStartRow][fStartCol-1] == 1 &&
+                            screen[fStartRow][fStartCol+fW] == 1 && screen[fStartRow+fH][fStartCol] == 1) {
+                            // first is found, trying to find second
+                            int sW = fW, sStartCol = fStartCol;
+                            for(int sH = 1; sH <= n; sH++) {
+                                for (int sStartRow = fStartRow + fH + 1; sStartRow <= n - sH + 1; sStartRow++) {
+                                    res = isFilledByVal(screen, n, sStartRow, sStartCol, sW, sH, 0);
+                                    if (res && screen[sStartRow][sStartCol-1] == 1) {
+                                        int copyOfScreen[][] = getScreenCopy(screen, n);
+                                        fillByVal(copyOfScreen, n, fStartRow, fStartCol, fW, fH, 1);
+                                        fillByVal(copyOfScreen, n, sStartRow, sStartCol, sW, sH, 1);
+                                        boolean hIsFound = isFilledAndCoverLikeH(copyOfScreen, n,
+                                            fStartRow, fStartCol, fW, fH,
+                                            sStartRow, sStartCol, sW, sH);
+                                        if (hIsFound) return true;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    boolean isFilledAndCoverLikeH(int[][] screen, int n,
+                                  int inFirstStartRow, int inFirstStartCol, int inFirstW, int inFirstH,
+                                  int inSecondStartRow, int inSecondStartCol, int inSecondW, int inSecondH) {
+        int sum = getSumOfScreen(screen, n);
+        for(int w = 1; w <= n; w++) {
+            for(int h = 1; h <= n; h++) {
+                for(int startRow = 1; startRow <= n - h + 1; startRow++) {
+                    for(int startCol = 1; startCol <= n - w + 1; startCol++) {
+                        boolean res = isFilledByValAndSum(screen, n, startRow, startCol, w, h, 1, sum);
+                        if (res) {
+                            int endRow = startRow + h - 1;
+                            int endCol = startCol + w - 1;
+                            int inEndRow = inSecondStartRow + inSecondH - 1;
+                            int inEndCol = inFirstStartCol + inFirstW - 1;
+                            return (startRow == inFirstStartRow && startCol < inFirstStartCol
+                                && endRow == inEndRow && endCol > inEndCol);
+                        }
+                    }
+                }
+            }
+        }
+        return false;
     }
 
     boolean isLetterL(int[][] screen, int n) {
@@ -188,6 +308,7 @@ public class LettersOnTheScreen {
 
     boolean isFilledByValAndSum(int[][] screen, int n, int startRow, int startCol, int w, int h, int val,
                                 int targetSum) {
+        if ((startRow + h - 1) > n || (startCol + w - 1) > n) return false;
         int filledByVal = 0;
         for(int r = startRow; r < startRow + h; r++) {
             for(int c = startCol; c < startCol + w; c++) {
@@ -201,6 +322,7 @@ public class LettersOnTheScreen {
     }
 
     boolean isFilledByVal(int[][] screen, int n, int startRow, int startCol, int w, int h, int val) {
+        if ((startRow + h - 1) > n || (startCol + w - 1) > n) return false;
         for(int r = startRow; r < startRow + h; r++) {
             for(int c = startCol; c < startCol + w; c++) {
                 if (screen[r][c] != val) {
