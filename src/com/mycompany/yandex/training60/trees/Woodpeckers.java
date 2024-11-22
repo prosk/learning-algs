db5185cb849612867ec49f8a1b870d5512e327cc
prosk <alexey.proskurnin@gmail.com>
2024-11-22 13:22:12 +0300

    yandex training 6.0 trees - Woodpeckers

--- src/com/mycompany/yandex/training60/trees/Woodpeckers.java
+++ src/com/mycompany/yandex/training60/trees/Woodpeckers.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.StringTokenizer;
 
@@ -15,6 +16,9 @@ public class Woodpeckers {
 
     int isolatedCnt = 0;
     int componentsCnt = 0;
+
+    int allLeftHouseCnt = 0;
+    int allRightHouseCnt = 0;
 
     List<Component> cmps = new ArrayList<>();
 
@@ -28,59 +32,88 @@ public class Woodpeckers {
         int M = readInt();
         int K = readInt();
         List<List<Integer>> adjList  = new ArrayList<>();
-        for (int i = 0; i <= N; i++) {
+        List<Vertex> vertexes = new ArrayList<>();
+        adjList.add(new ArrayList<>());
+        for (int i = 1; i <= N; i++) {
             adjList.add(new ArrayList<>());
+            vertexes.add(new Vertex(i, 0)); // index in list = vertex num - 1
         }
         // getting edges (u, v) and filling adj
         for (int i = 1; i <= M; i++) {
             int from = readInt();
             int to = readInt();
             addEdge(adjList, from, to);
+            vertexes.get(from-1).degree++;
+            vertexes.get(to-1).degree++;
         }
+        // sort vertexes by degree
+        Collections.sort(vertexes, (v1, v2) -> Integer.compare(v2.degree, v1.degree));
         // solve
         boolean[] visited = new boolean[N+1];
-        boolean hasCycle = false;
-        for(int i = 1; i <= N; i++) {
-            if (adjList.get(i).size() == 0) {
-                // изолированный дятел без друзей
-                isolatedCnt++;
-                continue;
+        boolean hasCorrectStructure = true;
+        // цикл по вершинам от максималььной степени к минимальной
+        for(int i = 0; i < vertexes.size(); i++) {
+            int vNum = vertexes.get(i).num;
+            if (adjList.get(vNum).size() == 0) {
+                // изолированный дятел без друзей, значит далее только изолированные
+                isolatedCnt = N - i;
+                break;
             }
-            if (!visited[i]) {
+            if (!visited[vNum]) {
                 // непосещенный компонент связности
                 componentsCnt++;
                 Component cmp = new Component();
                 cmp.type = CType.SEQUENCE; // если будет где-то 2 сына переприсвоится на TREE
                 cmps.add(cmp);
-                hasCycle = dfs(visited, adjList, i, 0, true, cmp);
-                if (hasCycle) break;
+                hasCorrectStructure = dfs(visited, adjList, vertexes,
+                        vNum, 0, true, cmp);
+                if (!hasCorrectStructure) break;
+                allLeftHouseCnt += cmp.leftHouseCnt;
+                allRightHouseCnt += cmp.rightHouseCnt;
             }
         }
-        if (hasCycle) {
+        if (!hasCorrectStructure) {
             out.println("0");
             return;
         }
     }
 
-    private boolean dfs(boolean[] visited, List<List<Integer>> adj,
+    private boolean dfs(boolean[] visited, List<List<Integer>> adj, List<Vertex> vertexes,
                      int fromNode, int parentNode, boolean isLeftHouse, Component cmp) {
+        if (visited[fromNode]) {
+            return false; // cycle
+        }
         visited[fromNode] = true;
         // increment house cnt for fromNode
         if (isLeftHouse) cmp.leftHouseCnt++; else cmp.rightHouseCnt++;
-
         int adjCnt = adj.get(fromNode).size();
-        int lowerCnt = 0;
-        long coinsSum = 0;
+
+        // проверяем корректность текущего "пучка" парент - чайлды
+        int childCnt = 0;
+        int childWithOwnChildCnt = 0;
+        int maxChildWithOwnChildCnt = (parentNode == 0) ? 2 : 1;
+        int freeChildCnt = 0;
         for (int i = 0; i < adjCnt; i++) {
             int childNode = adj.get(fromNode).get(i);
             if (childNode != parentNode) {
-                dfs(coins, vertexCnt, adj, childNode, fromNode);
-                lowerCnt += vertexCnt[childNode];
-                coinsSum += coins[childNode];
+                childCnt++;
+                boolean childHasOwnChilds = vertexes.get(childNode-1).degree > 1;
+                if (childHasOwnChilds) {
+                    childWithOwnChildCnt++;
+                } else {
+                    freeChildCnt++;
+                }
+                if (childWithOwnChildCnt > maxChildWithOwnChildCnt) {
+                    return false;
+                }
+                boolean hasCorrectStructure = dfs(visited, adj, vertexes, childNode, fromNode, !isLeftHouse, cmp);
+                if (!hasCorrectStructure) return false;
             }
         }
-        vertexCnt[fromNode] = lowerCnt+1;
-        coins[fromNode] = coinsSum + vertexCnt[fromNode];
+        // save local parent--childs combinations count
+
+        // if all checks is OK then hasCorrectStructure = true
+        return true;
     }
 
     private void addEdge(List<List<Integer>> adj, int i, int j)
@@ -110,10 +143,22 @@ public class Woodpeckers {
         }
     }
 
+    public static class Vertex {
+        int num;
+        int degree;
+
+        public Vertex(int num, int degree) {
+            this.num = num;
+            this.degree = degree;
+        }
+    }
+
     public static class Component {
         CType type;
         int leftHouseCnt;
         int rightHouseCnt;
+
+        int statesCnt;
     }
 
     public enum CType {
