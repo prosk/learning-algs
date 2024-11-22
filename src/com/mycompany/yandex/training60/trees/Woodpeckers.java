a8bd1d3b7ef708c2edc747578f0652cf6708708f
prosk <alexey.proskurnin@gmail.com>
2024-11-22 16:21:58 +0300

    yandex training 6.0 trees - Woodpeckers

--- src/com/mycompany/yandex/training60/trees/Woodpeckers.java
+++ src/com/mycompany/yandex/training60/trees/Woodpeckers.java
@@ -14,11 +14,16 @@ public class Woodpeckers {
     static PrintWriter out = new PrintWriter(System.out);
     static StringTokenizer tok = new StringTokenizer("");
 
+    static int SIZE = 0;
+    static int[] fact;
+
     int isolatedCnt = 0;
     int componentsCnt = 0;
 
     int allLeftHouseCnt = 0;
     int allRightHouseCnt = 0;
+
+    static int MODULO = 0;
 
     List<Component> cmps = new ArrayList<>();
 
@@ -31,6 +36,11 @@ public class Woodpeckers {
         int N = readInt();
         int M = readInt();
         int K = readInt();
+        MODULO = K;
+        SIZE = N;
+        fact = new int[SIZE + 1];
+        preFactorial();
+
         List<List<Integer>> adjList  = new ArrayList<>();
         List<Vertex> vertexes = new ArrayList<>();
         adjList.add(new ArrayList<>());
@@ -65,20 +75,43 @@ public class Woodpeckers {
                 Component cmp = new Component();
                 cmp.type = CType.SEQUENCE; // если будет где-то 2 сына переприсвоится на TREE
                 cmps.add(cmp);
-                hasCorrectStructure = dfs(visited, adjList, vertexes,
-                        vNum, 0, true, cmp);
+                hasCorrectStructure = dfs(visited, adjList, vNum, 0, true, cmp);
                 if (!hasCorrectStructure) break;
                 allLeftHouseCnt += cmp.leftHouseCnt;
                 allRightHouseCnt += cmp.rightHouseCnt;
+                // учет симметрии и поворота "сверху вниз"
+                if (cmp.leftHouseCnt == 1 || cmp.rightHouseCnt == 1) {
+                    // цепочка a--b либо пучок от 1 к N
+                    cmp.statesCnt = mult(cmp.statesCnt, 2);
+                } else {
+                    // возможны и симметрия и поворот сверху вниз
+                    cmp.statesCnt = mult(cmp.statesCnt, 4);
+                }
             }
         }
         if (!hasCorrectStructure) {
             out.println("0");
             return;
         }
+        // перебираем порядок компонентов и для каждого порядка все комбинации состояний
+        int cmpOrdersCnt = getFactorial(componentsCnt);
+        int statesCompCnt = 1;
+        for(Component cmp: cmps) {
+            statesCompCnt = mult(statesCompCnt, cmp.statesCnt);
+        }
+        int combForConnected = mult(cmpOrdersCnt, statesCompCnt);
+        // к зафиксированному порядку компонентов и состоянию каждого компонента
+        // добавляем перебор добавления к размещению изолированных дятлов
+        if (isolatedCnt > 0) {
+            int slots = isolatedCnt;
+            int delimiters = allLeftHouseCnt + allRightHouseCnt + 1;
+            int isolatedComb = binomCoeff(slots+delimiters, slots);
+            combForConnected = mult(combForConnected, isolatedComb);
+        }
+        out.println(combForConnected);
     }
 
-    private boolean dfs(boolean[] visited, List<List<Integer>> adj, List<Vertex> vertexes,
+    private boolean dfs(boolean[] visited, List<List<Integer>> adj,
                      int fromNode, int parentNode, boolean isLeftHouse, Component cmp) {
         if (visited[fromNode]) {
             return false; // cycle
@@ -97,7 +130,7 @@ public class Woodpeckers {
             int childNode = adj.get(fromNode).get(i);
             if (childNode != parentNode) {
                 childCnt++;
-                boolean childHasOwnChilds = vertexes.get(childNode-1).degree > 1;
+                boolean childHasOwnChilds = adj.get(childNode).size() > 1;
                 if (childHasOwnChilds) {
                     childWithOwnChildCnt++;
                 } else {
@@ -106,14 +139,37 @@ public class Woodpeckers {
                 if (childWithOwnChildCnt > maxChildWithOwnChildCnt) {
                     return false;
                 }
-                boolean hasCorrectStructure = dfs(visited, adj, vertexes, childNode, fromNode, !isLeftHouse, cmp);
+                boolean hasCorrectStructure = dfs(visited, adj, childNode, fromNode, !isLeftHouse, cmp);
                 if (!hasCorrectStructure) return false;
             }
         }
         // save local parent--childs combinations count
-
+        if (freeChildCnt > 0) {
+            cmp.statesCnt = mult(cmp.statesCnt, getFactorial(freeChildCnt));
+        }
         // if all checks is OK then hasCorrectStructure = true
         return true;
+    }
+
+    private static int mult(long a, long b) { return (int)((a * b) % MODULO); }
+    private static int sum(long a, long b) { return (int)((a + b) % MODULO); }
+
+    public static void preFactorial() {
+        fact[0] = 1;
+        for (int i = 1; i <= SIZE; i++) {
+            fact[i] = mult(i, fact[i - 1]);
+        }
+    }
+
+    public static int getFactorial(int a) {
+        if (a <= SIZE) {
+            return fact[a];
+        }
+        int res = fact[SIZE];
+        for (int i = SIZE+1; i <= a; i++) {
+            res = mult(i, res);
+        }
+        return res;
     }
 
     private void addEdge(List<List<Integer>> adj, int i, int j)
@@ -158,10 +214,22 @@ public class Woodpeckers {
         int leftHouseCnt;
         int rightHouseCnt;
 
-        int statesCnt;
+        int statesCnt = 1;
     }
 
     public enum CType {
         SEQUENCE, TREE
     }
+
+    static int binomCoeff(int n, int k) {
+        int[] pascalTriangleRow = new int[k + 1];
+
+        pascalTriangleRow[0] = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = Math.min(i, k); j > 0; j--)
+                pascalTriangleRow[j] = sum(pascalTriangleRow[j], pascalTriangleRow[j - 1]);
+        }
+        return pascalTriangleRow[k];
+    }
+
 }
