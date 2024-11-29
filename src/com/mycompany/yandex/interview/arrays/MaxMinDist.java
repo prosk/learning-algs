4c1ccd7f1d3c18eb4265865b1774e2e721b7310f
prosk <alexey.proskurnin@gmail.com>
2024-11-29 13:36:46 +0300

    yandex interview - MaxMinDist

---
+++ src/com/mycompany/yandex/interview/arrays/MaxMinDist.java
@@ -0,0 +1,176 @@
+package com.mycompany.yandex.interview.arrays;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.*;
+
+import static com.mycompany.yandex.interview.arrays.MaxMinDist.BuildingType.HOUSE;
+import static com.mycompany.yandex.interview.arrays.MaxMinDist.BuildingType.SHOP;
+
+public class MaxMinDist {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static PrintWriter out = new PrintWriter(System.out);
+    static StringTokenizer tok = new StringTokenizer("");
+
+    private final static Random rnd = new Random();
+
+    public static void main(String[] args) {
+        // new MaxMinDist().runTests();
+        new MaxMinDist().testForRandomArrays();
+        out.close();
+    }
+
+    void runTests() {
+        int dist = getMaxMinDist(new int[]{1, 0, 0, 1, 1, 0, 0, 2});
+        out.println(dist == 6 ? "OK" : "ERROR " + dist);
+
+        dist = getMaxMinDist(new int[]{1, 0, 0, 2, 1, 1, 0, 0, 0, 2, 1, 0 });
+        out.println(dist == 3 ? "OK" : "ERROR " + dist);
+
+        dist = getMaxMinDist(new int[]{1, 0, 0, 2, 1, 1, 0, 0,  0, 0, 0, 2, 1, 0 });
+        out.println(dist == 4 ? "OK" : "ERROR " + dist);
+
+        dist = getMaxMinDist(new int[]{1, 0, 0, 2, 1, 1, 0, 0, 0, 2, 1, 0, 1, 1, 1, 0, 0 });
+        out.println(dist == 7 ? "OK" : "ERROR " + dist);
+
+        dist = getMaxMinDist(new int[]{1, 0, 0, 2, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 2, 1, 0, 1, 1, 1, 0, 0 });
+        out.println(dist == 8 ? "OK" : "ERROR " + dist);
+
+        dist = getMaxMinDist(new int[]{1, 0, 0, 2, 1, 1, 0, 0, 0, 2, 1, 1, 0, 0, 0, 1, 0, 2, 0, 0, 1  });
+        out.println(dist == 4 ? "OK" : "ERROR " + dist);
+    }
+
+    /*
+     Дан массив из 0 1 2
+     2 - магазин
+     0 - жилой дом
+     1 - офис
+     Найти максимальное ближайшее расстояние от жилого дома до магазина
+
+     1 000 000 11 0 2 0 0 0 0 1 1 0 1 0 0 2 2 2 2 0 2 1 2 0 0 1 0 0 2 1 0 0 0 1 0 1
+
+     */
+
+    public enum BuildingType {
+        HOUSE(0),
+        OFFICE(1),
+        SHOP(2);
+
+        int value;
+
+        BuildingType(int value) {
+            this.value = value;
+        }
+    }
+
+    // betweenHouseIdx - индексы жилых домов, ограниченных 2-мя магазинами либо одним магазином и границей массива
+    // время O(N + общее число жилых домов, ограниченных магазинами слева и справа) = O(N)
+    // extra space = O( максимальное число жилых домов, ограниченных 2-мя магазинами либо магазином и границей массива)
+    int getMaxMinDist(int[] arr) {
+        int prevShopInd = -1, ans = 0;
+        List<Integer> betweenHousesIdx = new ArrayList<>();
+        for(int i = 0; i < arr.length; i++) {
+            if (arr[i] == SHOP.value && prevShopInd >= 0) {
+                int maxMin = 0;
+                for(int j: betweenHousesIdx)
+                    maxMin = Math.max(maxMin, Math.min(j - prevShopInd, i - j));
+                ans = Math.max(ans, maxMin);
+                prevShopInd = i;
+                betweenHousesIdx.clear();
+            } else if (arr[i] == SHOP.value && prevShopInd == -1) {
+                if (!betweenHousesIdx.isEmpty())
+                    ans = Math.max(ans, i - betweenHousesIdx.get(0));
+                prevShopInd = i;
+                betweenHousesIdx.clear();
+            } else if (arr[i] == HOUSE.value) {
+                betweenHousesIdx.add(i);
+            }
+        }
+        if (!betweenHousesIdx.isEmpty())
+            ans = Math.max(ans, betweenHousesIdx.get(betweenHousesIdx.size()-1) - prevShopInd);
+        return ans;
+    }
+
+    int getMaxMinDistBF(int[] arr) {
+        int ans = 0;
+        for(int i = 0; i < arr.length; i++) {
+            if (arr[i] == HOUSE.value) {
+                int minDistToShop = Integer.MAX_VALUE;
+                // to left
+                for(int j = i-1; j >= 0 ; j--) {
+                    if (arr[j] == SHOP.value) {
+                        minDistToShop = Math.min(minDistToShop, i - j);
+                        break;
+                    }
+                }
+                // to right
+                for(int j = i+1; j < arr.length ; j++) {
+                    if (arr[j] == SHOP.value) {
+                        minDistToShop = Math.min(minDistToShop, j - i);
+                        break;
+                    }
+                }
+                ans = Math.max(ans, minDistToShop);
+            }
+        }
+        return ans;
+    }
+
+    void testForRandomArrays() {
+        int testCount = 300, n = 100;
+        int successTestCnt = 0;
+        for(int i = 0; i < testCount; i++) {
+            int[] arr = new int[n];
+            boolean houses = false, shops = false;
+            for(int j = 0; j < n; j++) {
+                int val = rnd.nextInt(0, 3);
+                arr[j] = val;
+                houses = houses || val == HOUSE.value;
+                shops = shops || val == SHOP.value;
+            }
+            if (houses && shops) {
+                System.out.println("Test " + i);
+                System.out.println(Arrays.toString(arr));
+                int bfDist = getMaxMinDistBF(arr);
+                int dist = getMaxMinDist(arr);
+                if (bfDist == dist) {
+                    System.out.println("OK ans = " + dist);
+                    successTestCnt++;
+                } else {
+                    System.out.println("OK BF ans = " + bfDist + " my ans = " + dist);
+                }
+            }
+        }
+        System.out.println("ALL TESTS " + testCount);
+        System.out.println("SUCCESS TESTS " + successTestCnt);
+    }
+
+    void run() {
+        int a = readInt();
+        int ans = a + a;
+        out.println(ans);
+    }
+
+    int readInt() {
+        return Integer.parseInt(readString());
+    }
+
+    String readString() {
+        while (!tok.hasMoreTokens()) {
+            String nextLine = readLine();
+            if (null == nextLine) return null;
+            tok = new StringTokenizer(nextLine);
+        }
+        return tok.nextToken();
+    }
+
+    String readLine() {
+        try {
+            return br.readLine();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
