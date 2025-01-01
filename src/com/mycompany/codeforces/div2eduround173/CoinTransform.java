160f26d93bb12be09edbb60ffe20bed074357e4a
prosk <alexey.proskurnin@gmail.com>
2025-01-01 15:28:10 +0300

    codeforces dev2 edu round 173 - CoinTransform

---
+++ src/com/mycompany/codeforces/div2eduround173/CoinTransform.java
@@ -0,0 +1,61 @@
+package com.mycompany.codeforces.div2eduround173;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class CoinTransform {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static PrintWriter out = new PrintWriter(System.out);
+    static StringTokenizer tok = new StringTokenizer("");
+
+    public static void main(String[] args) {
+        new CoinTransform().run();
+        out.close();
+    }
+
+    void run() {
+        int t = readInt();
+        while (t > 0) {
+            solve();
+            t--;
+        }
+    }
+
+    void solve() {
+        long n = readLong();
+        long powVal = 4, ans = 1;
+        while(powVal <= n) {
+            ans *= 2;
+            powVal *= 4;
+        }
+        out.println(ans);
+    }
+
+    int readInt() {
+        return Integer.parseInt(readString());
+    }
+
+    long readLong() {
+        return Long.parseLong(readString());
+    }
+
+    String readString() {
+        while (!tok.hasMoreTokens()) {
+            String nextLine = readLine();
+            if (null == nextLine) return null;
+            tok = new StringTokenizer(nextLine);
+        }
+        return tok.nextToken();
+    }
+
+    String readLine() {
+        try {
+            return br.readLine();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
