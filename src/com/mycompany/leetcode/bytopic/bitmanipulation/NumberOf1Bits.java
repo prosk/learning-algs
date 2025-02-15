24d6b06718682396bdad6c31473801c3cf7a94bf
prosk <alexey.proskurnin@gmail.com>
2025-02-15 18:26:10 +0300

    leetcode - number of 1 bits

--- src/com/mycompany/leetcode/bytopic/bitmanipulation/NumberOf1Bits.java
+++ src/com/mycompany/leetcode/bytopic/bitmanipulation/NumberOf1Bits.java
@@ -1,9 +1,14 @@
 package com.mycompany.leetcode.bytopic.bitmanipulation;
 
 // https://leetcode.com/problems/number-of-1-bits/description/
+// решения ниже работают для любого целого n и положительного и отрицательного
+// чтобы get1BitsCnt работало для отрицательных используется сдвиг >>>
 public class NumberOf1Bits {
 
     public static void main(String[] args) {
+        System.out.println("-8 = " + Integer.toBinaryString(-8));
+        System.out.println("-9 = " + Integer.toBinaryString(-9));
+
         //System.out.println(get1BitsCnt(11));
         System.out.println(get1BitsCnt(-4569234));
         System.out.println(get1BitsCntOpt(-4569234));
