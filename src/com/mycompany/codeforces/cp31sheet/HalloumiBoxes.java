25c0f0930f9ab8503730d31c7c3f95cf914a6327
prosk <alexey.proskurnin@gmail.com>
2025-04-22 13:27:27 +0300

    codeforces - HalloumiBoxes

---
+++ src/com/mycompany/codeforces/cp31sheet/HalloumiBoxes.java
@@ -0,0 +1,68 @@
+package com.mycompany.codeforces.cp31sheet;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class HalloumiBoxes {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static PrintWriter out = new PrintWriter(System.out);
+    static StringTokenizer tok = new StringTokenizer("");
+
+    public static void main(String[] args) {
+        new HalloumiBoxes().run();
+        out.close();
+    }
+
+    void run() {
+        int t = readInt();
+        while (t > 0) {
+            solve();
+            t--;
+        }
+    }
+
+    void solve() {
+        int n = readInt();
+        int k = readInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++) arr[i] = readInt();
+
+        if (k >= 2 || n == 1) {
+            out.println("YES");
+            return;
+        }
+        // check for sorting
+        boolean sorted = true;
+        for(int i = 0; i < n-1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                sorted = false;
+                break;
+            }
+        }
+        out.println(sorted ? "YES" : "NO");
+    }
+
+    int readInt() {
+        return Integer.parseInt(readString());
+    }
+
+    String readString() {
+        while (!tok.hasMoreTokens()) {
+            String nextLine = readLine();
+            if (null == nextLine) return null;
+            tok = new StringTokenizer(nextLine);
+        }
+        return tok.nextToken();
+    }
+
+    String readLine() {
+        try {
+            return br.readLine();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
