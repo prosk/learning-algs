cb153c2b5f67810cee9bcbdf157acd189b2be880
alexey.proskurnin <alexey.proskurnin@gmail.com>
2025-11-08 16:15:09 +0300

    cp31 - CoverInWater - simple logic with obvious time complexity O(n)

--- src/com/mycompany/codeforces/cp31sheet/CoverInWater.java
+++ src/com/mycompany/codeforces/cp31sheet/CoverInWater.java
@@ -12,7 +12,7 @@ public class CoverInWater {
     static StringTokenizer tok = new StringTokenizer("");
 
     public static void main(String[] args) {
-        new CoverInWater().run();
+        new CoverInWater().runSimpleLogicWithObviousTimeComplexity();
         out.close();
     }
 
@@ -33,6 +33,34 @@ public class CoverInWater {
             }
             out.println(ans);
         }
+    }
+
+    /*
+       If there are 3 consecutive empty cells i−1, i, i+1, we can place water in cells i−1 and i+1
+       and then move water from cell i to all other cells.
+       If there are no such cells, we have to place water on every empty cell.
+
+       So if we find substring ''...'' in the array, the answer is 2,
+       otherwise the answer is the number of empty cells.
+
+       Time and memory complexities are O(N).
+     */
+    void runSimpleLogicWithObviousTimeComplexity() {
+        int t = readInt();
+        while(t-- > 0) {
+            int n = readInt();
+            String s = readString();
+            int ans = s.contains("...") ? 2 : charCnt(s, '.');
+            out.println(ans);
+        }
+    }
+
+    int charCnt(String s, char ch) {
+        int cnt = 0;
+        for(int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == ch) cnt++;
+        }
+        return cnt;
     }
 
     boolean isArraySorted(int[] arr) {
