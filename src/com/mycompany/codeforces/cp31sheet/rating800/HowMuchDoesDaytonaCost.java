02377dd3f3afd8eb734822c90a280a898a204b77
alexey.proskurnin <alexey.proskurnin@gmail.com>
2025-11-29 15:36:03 +0300

    cp31, rating 800, HowMuchDoesDaytonaCost

---
+++ src/com/mycompany/codeforces/cp31sheet/rating800/HowMuchDoesDaytonaCost.java
@@ -0,0 +1,59 @@
+package com.mycompany.codeforces.cp31sheet.rating800;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class HowMuchDoesDaytonaCost {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static PrintWriter out = new PrintWriter(System.out);
+    static StringTokenizer tok = new StringTokenizer("");
+
+    public static void main(String[] args) {
+        new HowMuchDoesDaytonaCost().run();
+        out.close();
+    }
+
+    void run() {
+        int t = readInt();
+        while (t > 0) {
+            solve();
+            t--;
+        }
+    }
+
+    void solve() {
+        int n = readInt();
+        int k = readInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++) arr[i] = readInt();
+
+        // Solution
+        boolean ans = Arrays.stream(arr).anyMatch(elem -> elem == k);
+        out.println(ans ? "YES" : "NO");
+    }
+
+    int readInt() {
+        return Integer.parseInt(readString());
+    }
+
+    String readString() {
+        while (!tok.hasMoreTokens()) {
+            String nextLine = readLine();
+            if (null == nextLine) return null;
+            tok = new StringTokenizer(nextLine);
+        }
+        return tok.nextToken();
+    }
+
+    String readLine() {
+        try {
+            return br.readLine();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
