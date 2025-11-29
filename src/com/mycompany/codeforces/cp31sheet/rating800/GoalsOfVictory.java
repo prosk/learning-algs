c64babfe6a9c3988e32529f2726943d5021b3127
alexey.proskurnin <alexey.proskurnin@gmail.com>
2025-11-29 16:15:24 +0300

    cp31, rating 800, GoalsOfVictory

---
+++ src/com/mycompany/codeforces/cp31sheet/rating800/GoalsOfVictory.java
@@ -0,0 +1,58 @@
+package com.mycompany.codeforces.cp31sheet.rating800;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class GoalsOfVictory {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static PrintWriter out = new PrintWriter(System.out);
+    static StringTokenizer tok = new StringTokenizer("");
+
+    public static void main(String[] args) {
+        new GoalsOfVictory().run();
+        out.close();
+    }
+
+    void run() {
+        int t = readInt();
+        while (t > 0) {
+            solve();
+            t--;
+        }
+    }
+
+    void solve() {
+        int n = readInt();
+        int[] arr = new int[n-1];
+        for(int i = 0; i < n-1; i++) arr[i] = readInt();
+
+        // Solution
+        int ans = -1 * Arrays.stream(arr).sum();
+        out.println(ans);
+    }
+
+    int readInt() {
+        return Integer.parseInt(readString());
+    }
+
+    String readString() {
+        while (!tok.hasMoreTokens()) {
+            String nextLine = readLine();
+            if (null == nextLine) return null;
+            tok = new StringTokenizer(nextLine);
+        }
+        return tok.nextToken();
+    }
+
+    String readLine() {
+        try {
+            return br.readLine();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
