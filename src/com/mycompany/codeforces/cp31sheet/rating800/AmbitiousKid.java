f69240fa6e8c40bb566645de65d234d2a64dcbd5
alexey.proskurnin <alexey.proskurnin@gmail.com>
2025-11-29 17:20:56 +0300

    cp31, rating 800, AmbitiousKid

---
+++ src/com/mycompany/codeforces/cp31sheet/rating800/AmbitiousKid.java
@@ -0,0 +1,57 @@
+package com.mycompany.codeforces.cp31sheet.rating800;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class AmbitiousKid {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static PrintWriter out = new PrintWriter(System.out);
+    static StringTokenizer tok = new StringTokenizer("");
+
+    public static void main(String[] args) {
+        new AmbitiousKid().run();
+        out.close();
+    }
+
+    void run() {
+        solve();
+    }
+
+    void solve() {
+        int n = readInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++) arr[i] = readInt();
+
+        // solution
+        int minDiffToZero = Integer.MAX_VALUE;
+        for(int i = 0; i < n; i++) {
+            int diffToZero = Math.abs(arr[i]);
+            if (diffToZero < minDiffToZero) minDiffToZero = diffToZero;
+        }
+        out.println(minDiffToZero);
+    }
+
+    int readInt() {
+        return Integer.parseInt(readString());
+    }
+
+    String readString() {
+        while (!tok.hasMoreTokens()) {
+            String nextLine = readLine();
+            if (null == nextLine) return null;
+            tok = new StringTokenizer(nextLine);
+        }
+        return tok.nextToken();
+    }
+
+    String readLine() {
+        try {
+            return br.readLine();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
