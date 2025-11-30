0c3ace640d7091b0766d88f3c0124dd08ad16cda
alexey.proskurnin <alexey.proskurnin@gmail.com>
2025-11-30 15:18:37 +0300

    cp31, rating 800, Buttons

---
+++ src/com/mycompany/codeforces/cp31sheet/rating800/Buttons.java
@@ -0,0 +1,59 @@
+package com.mycompany.codeforces.cp31sheet.rating800;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class Buttons {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static PrintWriter out = new PrintWriter(System.out);
+    static StringTokenizer tok = new StringTokenizer("");
+
+    public static void main(String[] args) {
+        new Buttons().run();
+        out.close();
+    }
+
+    void run() {
+        int t = readInt();
+        while (t > 0) {
+            solve();
+            t--;
+        }
+    }
+
+    void solve() {
+        int a = readInt();
+        int b = readInt();
+        int c = readInt();
+        // solution
+        int firstCnt = a + (c + 1)/2;
+        int secondCnt = b + c/2;
+        String ans = (secondCnt >= firstCnt) ? "Second" : "First";
+        out.println(ans);
+    }
+
+    int readInt() {
+        return Integer.parseInt(readString());
+    }
+
+    String readString() {
+        while (!tok.hasMoreTokens()) {
+            String nextLine = readLine();
+            if (null == nextLine) return null;
+            tok = new StringTokenizer(nextLine);
+        }
+        return tok.nextToken();
+    }
+
+    String readLine() {
+        try {
+            return br.readLine();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
