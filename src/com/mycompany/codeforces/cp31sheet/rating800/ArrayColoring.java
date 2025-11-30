ba6fa150d975c08f7dad0e0c1645d778b7143128
alexey.proskurnin <alexey.proskurnin@gmail.com>
2025-11-30 15:44:40 +0300

    cp31, rating 800, ArrayColoring

---
+++ src/com/mycompany/codeforces/cp31sheet/rating800/ArrayColoring.java
@@ -0,0 +1,60 @@
+package com.mycompany.codeforces.cp31sheet.rating800;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class ArrayColoring {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static PrintWriter out = new PrintWriter(System.out);
+    static StringTokenizer tok = new StringTokenizer("");
+
+    public static void main(String[] args) {
+        new ArrayColoring().run();
+        out.close();
+    }
+
+    void run() {
+        int t = readInt();
+        while (t > 0) {
+            solve();
+            t--;
+        }
+    }
+
+    void solve() {
+        int n = readInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++) arr[i] = readInt();
+
+        // solution
+        int oddsCnt = 0;
+        for(int i = 0; i < n; i++) {
+            oddsCnt += (arr[i] % 2 == 1) ? 1 : 0;
+        }
+        out.println((oddsCnt % 2 == 0) ? "YES" : "NO");
+    }
+
+    int readInt() {
+        return Integer.parseInt(readString());
+    }
+
+    String readString() {
+        while (!tok.hasMoreTokens()) {
+            String nextLine = readLine();
+            if (null == nextLine) return null;
+            tok = new StringTokenizer(nextLine);
+        }
+        return tok.nextToken();
+    }
+
+    String readLine() {
+        try {
+            return br.readLine();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
