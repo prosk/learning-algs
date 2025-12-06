e9811713865ea3791ae252b883d73391d113773b
alexey.proskurnin <alexey.proskurnin@gmail.com>
2025-12-06 13:24:35 +0300

    cp31, rating 800, Coins

---
+++ src/com/mycompany/codeforces/cp31sheet/rating800/Coins.java
@@ -0,0 +1,57 @@
+package com.mycompany.codeforces.cp31sheet.rating800;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class Coins {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static PrintWriter out = new PrintWriter(System.out);
+    static StringTokenizer tok = new StringTokenizer("");
+
+    public static void main(String[] args) {
+        new Coins().run();
+        out.close();
+    }
+
+    void run() {
+        int t = readInt();
+        while (t-- > 0) {
+            solve();
+        }
+    }
+
+    void solve() {
+        long n = readLong();
+        long k = readLong();
+        String ans = (n % 2 == 0L || n % k == 0L || k % 2 == 1L) ? "YES" : "NO";
+        out.println(ans);
+    }
+
+    int readInt() {
+        return Integer.parseInt(readString());
+    }
+
+    long readLong() {
+        return Long.parseLong(readString());
+    }
+
+    String readString() {
+        while (!tok.hasMoreTokens()) {
+            String nextLine = readLine();
+            if (null == nextLine) return null;
+            tok = new StringTokenizer(nextLine);
+        }
+        return tok.nextToken();
+    }
+
+    String readLine() {
+        try {
+            return br.readLine();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
