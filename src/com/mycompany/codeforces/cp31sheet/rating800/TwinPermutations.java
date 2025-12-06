f4b1c69804d65da546006a85543af2055fad4fb9
alexey.proskurnin <alexey.proskurnin@gmail.com>
2025-12-06 11:36:03 +0300

    cp31, rating 800, TwinPermutations

---
+++ src/com/mycompany/codeforces/cp31sheet/rating800/TwinPermutations.java
@@ -0,0 +1,60 @@
+package com.mycompany.codeforces.cp31sheet.rating800;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class TwinPermutations {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static PrintWriter out = new PrintWriter(System.out);
+    static StringTokenizer tok = new StringTokenizer("");
+
+    public static void main(String[] args) {
+        new TwinPermutations().run();
+        out.close();
+    }
+
+    void run() {
+        int t = readInt();
+        while (t-- > 0) {
+            solve();
+        }
+    }
+
+    void solve() {
+        int n = readInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++) {
+            arr[i] = readInt();
+        }
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < n; i++) {
+            if (i > 0) sb.append(' ');
+            sb.append(n + 1 - arr[i]);
+        }
+        out.println(sb);
+    }
+
+    int readInt() {
+        return Integer.parseInt(readString());
+    }
+
+    String readString() {
+        while (!tok.hasMoreTokens()) {
+            String nextLine = readLine();
+            if (null == nextLine) return null;
+            tok = new StringTokenizer(nextLine);
+        }
+        return tok.nextToken();
+    }
+
+    String readLine() {
+        try {
+            return br.readLine();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
