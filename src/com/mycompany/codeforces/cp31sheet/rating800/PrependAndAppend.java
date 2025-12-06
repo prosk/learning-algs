44da3b27f234cc96e7fc8a6889bcdbe520c14c72
alexey.proskurnin <alexey.proskurnin@gmail.com>
2025-12-06 18:13:18 +0300

    cp31 - 800 - PrependAndAppend

---
+++ src/com/mycompany/codeforces/cp31sheet/rating800/PrependAndAppend.java
@@ -0,0 +1,59 @@
+package com.mycompany.codeforces.cp31sheet.rating800;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class PrependAndAppend {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static PrintWriter out = new PrintWriter(System.out);
+    static StringTokenizer tok = new StringTokenizer("");
+
+    public static void main(String[] args) {
+        new PrependAndAppend().run();
+        out.close();
+    }
+
+    void run() {
+        int t = readInt();
+        while (t-- > 0) {
+            solve();
+        }
+    }
+
+    void solve() {
+        int n = readInt();
+        String s = readString();
+        // solution
+        int i = 0, j = n-1;
+        while(n > 1 && s.charAt(i) != s.charAt(j)) {
+            i++;
+            j--;
+            n -= 2;
+        }
+        out.println(n);
+    }
+
+    int readInt() {
+        return Integer.parseInt(readString());
+    }
+
+    String readString() {
+        while (!tok.hasMoreTokens()) {
+            String nextLine = readLine();
+            if (null == nextLine) return null;
+            tok = new StringTokenizer(nextLine);
+        }
+        return tok.nextToken();
+    }
+
+    String readLine() {
+        try {
+            return br.readLine();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
