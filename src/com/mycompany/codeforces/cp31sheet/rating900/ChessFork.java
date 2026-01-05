f063a9a9cf6dfdbe9f1cd11ce7b108197e1961cc
alexey.proskurnin <alexey.proskurnin@gmail.com>
2026-01-05 15:09:33 +0300

    cp31 - 900 - ChessFork - super clean solution without sets

--- src/com/mycompany/codeforces/cp31sheet/rating900/ChessFork.java
+++ src/com/mycompany/codeforces/cp31sheet/rating900/ChessFork.java
@@ -7,6 +7,29 @@ import java.io.PrintWriter;
 import java.util.*;
 
 /*
+   Super clean solution from winner shenzihan (without sets and intersections, just counting)
+
+       int main() {
+        ios::sync_with_stdio(0);
+        cin.tie(0);
+        cin >> T;
+        while (T--) {
+            int a, b, x, y, z, w;
+            cin >> a >> b >> x >> y >> z >> w;
+            const int dx[8] = {a, a, -a, -a, b, b, -b, -b};
+            const int dy[8] = {b, -b, b, -b, a, -a, a, -a};
+            int ans = 0;
+            for (int i = 0; i < (a == b ? 4 : 8); ++i) {
+                int px = x + dx[i], py = y + dy[i];
+                int tx = abs(px - z), ty = abs(py - w);
+                if ((tx == a && ty == b) || (tx == b && ty == a)) ++ans;
+            }
+            cout << ans << '\n';
+        }
+        return 0;
+    }
+
+
    There are at most 8 positions of the knight that can attack a single cell.
    Therefore, we can find all 8 positions that attack the king and the 8 positions that attack the queen
    and count the number of positions that appear in both of these lists.
